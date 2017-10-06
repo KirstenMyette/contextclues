a653f328d2b77e05200363e919c0bda39f1fea4b
kirstenmyette <kirstenmyette@aol.com>
2017-10-06 12:32:01 -0500

    incomplete lab. i think i have a problem understanding javascript and or jquery

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
         var clickMe = $(document.createElement('h3'));
         $('body').append(clickMe);
 
-
+//for the love of god why doesn't this work???
         $('clickMe').click(function () {
         alert(accusation.bind(obj));
 
@@ -37,3 +37,20 @@ $(document).ready(function () {
 
             
 
+// original starting point
+
+// var name = ['Kiera', 'Tiffany', 'Jeff', 'Erica', 'Brandon'];
+// var place = ['the kitchen', 'the bathroom', 'the bedroom', 'the parlor', 'the backyard', 'the library', 'the laundry room', 'the living room', 'the garage', 'the frontyard'];
+// var weapon = ['lightsaber', 'landmine', 'katana', 'hatchet', 'gun', 'grenade', 'heat seeking missile', 'ninja throwing star', 'rope', 'spoon', 'thumbtack', 'childproof scissors', 'the zombie virus', 'vampire bite', 'toothpick', 'desktop pc', 'Samsung Galaxy 8', 'Deadpool 4th wall break', 'wet, angry cat', 'baseball bat'];
+
+
+// // 'Accusation ${i}: I accuse ${this.name}, with a ${this.weapon}, in the ${this.place}!'
+
+// $(document).ready(function () {
+//     for (var i = 1; i < 100; i++) {
+//         $('body').append($(`<h3>Accusation ${i}</h3>`));
+//     }
+//     $('h3').click(function () {
+//         alert('deadpool sucks!');
+//     });
+// });
